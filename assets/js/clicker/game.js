83836c2a16d190be9bc76812775f4c3619726279
CalvinWilliams1012 <calvin.william1012@gmail.com>
2017-10-13 12:51:20 -0400

    Started working on incremental game

---
+++ assets/js/clicker/game.js
@@ -0,0 +1,18 @@
+var Game = {
+	/* Game version, not usefull now, might be in the future.*/
+	version: 0.01,
+	/* Main currency of the game. */
+	frogs: 0,
+
+	/* -------------------------------GENERATORS------------------------------- */
+	/* */
+	clickers: 0,
+	/* ------------------------------------------------------------------------ */
+
+	/* This function is the *main* function in which the incremental game increments.
+	This function is called by using a setInterval timer and based on what the user has increments there score.*/
+	function update() {
+		//TODO: update score based on generators
+	},
+
+}
